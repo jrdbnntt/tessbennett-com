0f39476eb60a2aa1312a77337bed7e01a54d5282
jrdbnntt <jrdbnntt@gmail.com>
2017-07-10 21:30:17 -0400

    requested edits + 3 new images

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,21 @@
  * Gulp Configuration
  */
 
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var rename = require('gulp-rename');
-var pug = require('gulp-pug');
-var header = require('gulp-header');
-var bower = require('gulp-bower');
-var ghPages = require('gulp-gh-pages');
-var imageResize = require('gulp-image-resize');
+var gulp = require('gulp')
+var uglify = require('gulp-uglify')
+var sourcemaps = require('gulp-sourcemaps')
+var sass = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer')
+var rename = require('gulp-rename')
+var pug = require('gulp-pug')
+var header = require('gulp-header')
+var bower = require('gulp-bower')
+var ghPages = require('gulp-gh-pages')
+var imageResize = require('gulp-image-resize')
 
-var pkg = require('./package.json');
-
+var pkg = require('./package.json')
 
 var banner =
     '/** \n' +
@@ -25,171 +24,162 @@ var banner =
     ' * @version v<%= pkg.version %>\n' +
     ' * @link <%= pkg.homepage %>\n' +
     ' * @license <%= pkg.license %>\n' +
-    ' */\n\n';
+    ' */\n\n'
 
 var dirs = {
-    build: __dirname + '/build',
-    src: __dirname + '/src',
-    cloud: __dirname + '/cloud'
-};
+  build: __dirname + '/build',
+  src: __dirname + '/src',
+  cloud: __dirname + '/cloud'
+}
 
 /**
  * Static files
  */
-gulp.task('static', function() {
-    return gulp.src(dirs.src + '/static/**/*').pipe(gulp.dest(dirs.build));
-});
-
+gulp.task('static', function () {
+  return gulp.src(dirs.src + '/static/**/*').pipe(gulp.dest(dirs.build))
+})
 
 /**
  * CSS
  */
-gulp.task('css', function() {
-    return gulp.src([
-        dirs.src + '/css/**/*.scss',
-        '!' + dirs.src + '/css/_*',
-        '!' + dirs.src + '/css/_*/**'
-    ])
-        .pipe(sourcemaps.init())
-        .pipe(sass.sync({
-            outputStyle: 'compact',
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-        .pipe(sourcemaps.write('./',{
-            addComment: true,
-            includeContent: true
-        }))
-        .pipe(gulp.dest(dirs.build + '/css'));
-});
-
+gulp.task('css', function () {
+  return gulp.src([
+    dirs.src + '/css/**/*.scss',
+    '!' + dirs.src + '/css/_*',
+    '!' + dirs.src + '/css/_*/**'
+  ])
+  .pipe(sourcemaps.init())
+  .pipe(sass.sync({
+    outputStyle: 'compact'
+  }).on('error', sass.logError))
+  .pipe(autoprefixer({
+    cascade: false
+  }))
+  .pipe(sourcemaps.write('./', {
+    addComment: true,
+    includeContent: true
+  }))
+  .pipe(gulp.dest(dirs.build + '/css'))
+})
 
 /**
  * HTML
  */
-gulp.task('html', function() {
-    return gulp.src([
-        dirs.src + '/html/**/*.pug',
-        '!' + dirs.src + '/html/_*',
-        '!' + dirs.src + '/html/_*/**'
-    ])
-        .pipe(pug({
-            pretty: true,
-            locals: require(dirs.src + '/html/locals.js')
-        }))
-        .pipe(gulp.dest(dirs.build));
-});
-
+gulp.task('html', function () {
+  return gulp.src([
+    dirs.src + '/html/**/*.pug',
+    '!' + dirs.src + '/html/_*',
+    '!' + dirs.src + '/html/_*/**'
+  ])
+  .pipe(pug({
+    pretty: true,
+    locals: require(dirs.src + '/html/locals.js')
+  }))
+  .pipe(gulp.dest(dirs.build))
+})
 
 /**
  * JS
  */
-gulp.task('js', function() {
-    return gulp.src([
-        dirs.src + '/js/**/*.js'
-    ])
-        .pipe(sourcemaps.init())
-        .pipe(header(banner, {pkg : pkg}))
-        .pipe(rename(function(path) {
-            path.extname = '.min.js';
-        }))
-        .pipe(uglify({
-            mangle: true,
-            compress: true
-        }))
-        .pipe(sourcemaps.write('./', {
-            addComment: true,
-            includeContent: true
-        }))
-        .pipe(gulp.dest(dirs.build + '/js'));
-});
-
+gulp.task('js', function () {
+  return gulp.src([
+    dirs.src + '/js/**/*.js'
+  ])
+  .pipe(sourcemaps.init())
+  .pipe(header(banner, {pkg: pkg}))
+  .pipe(rename(function (path) {
+    path.extname = '.min.js'
+  }))
+  .pipe(uglify({
+    mangle: true,
+    compress: true
+  }))
+  .pipe(sourcemaps.write('./', {
+    addComment: true,
+    includeContent: true
+  }))
+  .pipe(gulp.dest(dirs.build + '/js'))
+})
 
 /**
  * Resize base images to create needed sizes
  */
-gulp.task('img:resize', function() {
-    return gulp.src(dirs.cloud + '/img/originals/**/*.jpg')
-        .pipe(rename(function(path) {
-            path.basename = path.basename.replace(/\s/g, '_').toLowerCase();
-        }))
-        .pipe(imageResize({
-            width: 500,
-            height: 1000,
-            crop: false,
-            upscale: false,
-            quality: 1,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest(dirs.build + '/img/thumbs'));
-});
-gulp.task('img:copy', function() {
-    return gulp.src(dirs.cloud + '/img/**/*.jpg')
-        .pipe(rename(function(path) {
-            path.basename = path.basename.replace(/\s/g, '_').toLowerCase();
-        }))
-        .pipe(gulp.dest(dirs.build + '/img'));
-});
+gulp.task('img:resize', function () {
+  return gulp.src(dirs.cloud + '/img/originals/**/*.jpg')
+  .pipe(rename(function (path) {
+    path.basename = path.basename.replace(/\s/g, '_').toLowerCase()
+  }))
+  .pipe(imageResize({
+    width: 500,
+    height: 1000,
+    crop: false,
+    upscale: false,
+    quality: 1,
+    imageMagick: true
+  }))
+  .pipe(gulp.dest(dirs.build + '/img/thumbs'))
+})
+gulp.task('img:copy', function () {
+  return gulp.src(dirs.cloud + '/img/**/*.jpg')
+  .pipe(rename(function (path) {
+    path.basename = path.basename.replace(/\s/g, '_').toLowerCase()
+  }))
+  .pipe(gulp.dest(dirs.build + '/img'))
+})
 
-gulp.task('img', ['img:copy', 'img:resize']);
+gulp.task('img', ['img:copy', 'img:resize'])
 
+gulp.task('bower', function () {
+  return bower({
+    directory: dirs.build + '/bower_components'
+  })
+})
 
-gulp.task('bower', function() {
-    return bower({
-        directory: dirs.build + '/bower_components'
-    });
-});
+gulp.task('build', ['bower', 'css', 'html', 'js', 'static'])
 
-
-gulp.task('build', ['bower', 'css', 'html', 'js', 'static']);
-
-gulp.task('watch', ['build'], function(){
-    return gulp.watch([dirs.src + '/**/*'], ['build']);
-});
-
+gulp.task('watch', ['build'], function () {
+  return gulp.watch([dirs.src + '/**/*'], ['build'])
+})
 
 /**
  * Builds website and then deploys result to gitub pages branch
  */
-gulp.task('deploy', ['build', 'img', 'bower'], function() {
-    return gulp.src(dirs.build + '/**/*')
-        .pipe(ghPages({
-            remoteUrl: pkg.repository,
-            force: true
-        }));
-});
-
+gulp.task('deploy', ['build', 'img', 'bower'], function () {
+  return gulp.src(dirs.build + '/**/*')
+  .pipe(ghPages({
+    remoteUrl: pkg.repository,
+    force: true
+  }))
+})
 
 /**
  * Demo server. Basic static express app pointing to the build folder
  */
-gulp.task('demo', function(done) {
-    var express = require('express');
-    var http = require('http');
-    var morgan = require('morgan');
+gulp.task('demo', function (done) {
+  var express = require('express')
+  var http = require('http')
+  var morgan = require('morgan')
 
-    var port = 5000;
+  var port = 5000
 
-    var app = express();
-    app.use(morgan('dev'));
-    app.use('/', express.static(dirs.build));
-    app.get('/', function(req, res) {
-        res.redirect('/index.html');
-    });
+  var app = express()
+  app.use(morgan('dev'))
+  app.use('/', express.static(dirs.build))
+  app.get('/', function (req, res) {
+    res.redirect('/index.html')
+  })
 
-    var server = http.createServer(app);
-    server.on('error', function(error) {
-        console.log('Server error!', error);
-        done(error);
-    });
-    server.on('listening', function() {
-        console.log('Express app hosting static directory ', dirs.build);
-        console.log('Listening on localhost ' + port);
-        done();
-    });
-    server.listen(port);
-});
+  var server = http.createServer(app)
+  server.on('error', function (error) {
+    console.log('Server error!', error)
+    done(error)
+  })
+  server.on('listening', function () {
+    console.log('Express app hosting static directory ', dirs.build)
+    console.log('Listening on localhost ' + port)
+    done()
+  })
+  server.listen(port)
+})
 
-
-gulp.task('default', ['build']);
+gulp.task('default', ['build'])
--- src/html/locals.js
+++ src/html/locals.js
@@ -2,98 +2,98 @@
  * Local Pug Data
  */
 
-var locals = {};
+var locals = {}
 
-locals.baseTitle = 'Tess Bennett';
-locals.getTitle = function(subtitle) {
-    if (subtitle) {
-        return locals.baseTitle + ' - ' + subtitle;
-    }
-    return locals.baseTitle;
-};
-
+locals.baseTitle = 'Tess Bennett'
+locals.getTitle = function (subtitle) {
+  if (subtitle) {
+    return locals.baseTitle + ' - ' + subtitle
+  }
+  return locals.baseTitle
+}
 
 locals.works = {
-    new_portraits: {
-        title: 'New portraits',
-        link: '/works/new_portraits.html',
-        imageRootSrc: 'new_portraits',
-        images: [
+  pair: {
+    title: 'Pair',
+    link: '/works/pair.html',
+    imageRootSrc: 'pair',
+    images: [
+      {src: 'clouds_looked_like.jpg', title: 'what clouds looked like', description: '2016. Watercolor and graphite on paper, 51 x 78 inches.'},
+      {src: 'time_spent.jpg', title: 'time spent walking home', description: '2016. Watercolor and graphite on paper, 51 x 88 inches.'},
+      {src: 'clouds_looked_like_detail_one.jpg', beforeTitle: 'Detail of', title: 'what clouds looked like'},
+      {src: 'clouds_looked_like_detail_two.jpg', beforeTitle: 'Detail of', title: 'what clouds looked like'},
+      {src: 'time_spent_detail_one.jpg', beforeTitle: 'Detail of', title: 'time spent walking home'},
+      {src: 'time_spent_detail_two.jpg', beforeTitle: 'Detail of', title: 'time spent walking home'},
+      {src: 'pair.jpg'}
+    ]
+  },
 
-            {src: 'clouds_looked_like.jpg', title: 'what clouds looked like', description: '2016. Watercolor and graphite on paper, 51 x 78 inches.'},
-            {src: 'time_spent.jpg', title: 'time spent walking home', description: '2016. Watercolor and graphite on paper, 51 x 88 inches.'},
-            {src: 'clouds_looked_like_detail_one.jpg', beforeTitle: 'Detail of', title: 'what clouds looked like', description: '2016. Watercolor and graphite.'},
-            {src: 'clouds_looked_like_detail_two.jpg', beforeTitle: 'Detail of', title: 'what clouds looked like', description: '2016. Watercolor and graphite.'},
-            {src: 'time_spent_detail_one.jpg', beforeTitle: 'Detail of', title: 'time spent walking home', description: '2016. Watercolor and graphite.'},
-            {src: 'time_spent_detail_two.jpg', beforeTitle: 'Detail of', title: 'time spent walking home', description: '2016. Watercolor and graphite.'}
-        ]
-    },
+  cindy: {
+    title: 'Cindy',
+    link: '/works/cindy.html',
+    imageRootSrc: 'cindy',
+    images: [
+      {src: 'cindy_no5.jpg', title: 'Cindy (no. 5)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
+      {src: 'cindy_no3.jpg', title: 'Cindy (no. 3)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
+      {src: 'cindy_no4.jpg', title: 'Cindy (no. 4)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
+      {src: 'cindy_no2.jpg', title: 'Cindy (no. 2)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
+      {src: 'cindy_no1.jpg', title: 'Cindy (no. 1)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
+      {src: 'cindy.jpg', title: 'Cindy', description: '2015. Watercolor and graphite on paper.'}
+    ]
+  },
 
-    cindy: {
-        title: 'Cindy',
-        link: '/works/cindy.html',
-        imageRootSrc: 'cindy',
-        images: [
-            {src: 'cindy_no5.jpg', title: 'Cindy (no. 5)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
-            {src: 'cindy_no3.jpg', title: 'Cindy (no. 3)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
-            {src: 'cindy_no4.jpg', title: 'Cindy (no. 4)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
-            {src: 'cindy_no2.jpg', title: 'Cindy (no. 2)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
-            {src: 'cindy_no1.jpg', title: 'Cindy (no. 1)', description: '2015. Watercolor and graphite on paper, 25 x 40 inches.'},
-            {src: 'cindy.jpg', title: 'Cindy', description: '2015. Watercolor and graphite on paper.'}
-        ]
-    },
+  transfers: {
+    title: 'Transfers',
+    link: '/works/transfers.html',
+    imageRootSrc: 'transfers',
+    images: [
+      {src: 'transfers.jpg', title: 'Transfers', description: '2015. Graphite transfers.'},
+      {src: 'transfers_stairs_that_lead_to_nowhere.jpg', title: 'Stairs that lead to nowhere (cropped version)', description: '2015. Graphite transfers on paper, 40 x 60 inches.'},
+      {src: 'transfers_empty_picture_frames.jpg', title: 'Empty frames that hang in the yard', description: '2015. Graphite transfers on paper, 40 x 60 inches.'},
+      {src: 'transfers_plates_1.jpg', title: 'Transfers (plates)', description: '2015. Graphite on tracing paper.'},
+      {src: 'transfers_plates_2.jpg', title: 'Transfers (plates)', description: '2015. Graphite on tracing paper.'}
+    ]
+  },
 
-    transfers: {
-        title: 'Transfers',
-        link: '/works/transfers.html',
-        imageRootSrc: 'transfers',
-        images: [
-            {src: 'transfers.jpg', title: 'Transfers', description: '2015. Graphite transfers.'},
-            {src: 'transfers_stairs_that_lead_to_nowhere.jpg', title: 'Stairs that lead to nowhere (cropped version)', description: '2015. Graphite transfers, 40 x 60 inches.'},
-            {src: 'transfers_empty_picture_frames.jpg', title: 'Empty picture frames (cropped version)', description: '2015. Graphite transfers, 40 x 60 inches.'},
-            {src: 'transfers_plates_1.jpg', title: 'Transfers (plates)', description: '2015. Graphite on tracing paper.'},
-            {src: 'transfers_plates_2.jpg', title: 'Transfers (plates)', description: '2015. Graphite on tracing paper.'}
-        ]
-    },
+  one_week: {
+    title: 'One week',
+    link: '/works/one_week.html',
+    imageRootSrc: 'one_week',
+    images: [
+      {src: 'one_week_no7.jpg', title: 'One week (no. 7)', description: '2014. Watercolor and graphite on paper, 22 x 30 inches.'},
+      {src: 'one_week_no1.jpg', title: 'One week (no. 1)', description: '2014. Watercolor and graphite on paper, 22 x 30 inches.'},
+      {src: 'one_week_no4.jpg', title: 'One week (no. 4)', description: '2014. Watercolor and graphite on paper, 22 x 30 inches.'},
+      {src: 'one_week_no6.jpg', title: 'One week (no. 6)', description: '2014. Watercolor and graphite on paper, 22 x 30 inches.'},
+      {src: 'one_week.jpg', title: 'One week', description: '2014. Watercolor and graphite.'}
+    ]
+  },
 
-    one_week: {
-        title: 'One week',
-        link: '/works/one_week.html',
-        imageRootSrc: 'one_week',
-        images: [
-            {src: 'one_week_no7.jpg', title: 'One week (no. 7)', description: '2014. Watercolor and graphite on paper, 22 x 30 inches.'},
-            {src: 'one_week_no1.jpg', title: 'One week (no. 1)', description: '2014. Watercolor and graphite on paper, 22 x 30 inches.'},
-            {src: 'one_week_no4.jpg', title: 'One week (no. 4)', description: '2014. Watercolor and graphite on paper, 22 x 30 inches.'},
-            {src: 'one_week_no6.jpg', title: 'One week (no. 6)', description: '2014. Watercolor and graphite on paper, 22 x 30 inches.'},
-            {src: 'one_week.jpg', title: 'One week', description: '2014. Watercolor and graphite.'}
-        ]
-    },
+  erased_drawings: {
+    title: 'Erased drawings',
+    link: '/works/erased_drawings.html',
+    imageRootSrc: 'erased_drawings',
+    images: [
+      {src: 'erased_drawings_no4.jpg', title: 'Erased drawings (no. 4)', description: '2014. Graphite, tape, and walnut oil on paper, 18 x 24 inches.'},
+      {src: 'erased_drawings_no7.jpg', title: 'Erased drawings (no. 7)', description: '2014. Graphite and tape on paper, 18 x 24 inches.'},
+      {src: 'erased_drawings_no6.jpg', title: 'Erased drawings (no. 6)', description: '2014. Graphite and tape on paper, 18 x 24 inches.'},
+      {src: 'erased_drawings_no2.jpg', title: 'Erased drawings (no. 2)', description: '2014. Graphite, tape, and walnut oil on paper, 18 x 24 inches.'}
+    ]
+  },
 
-    erased_drawings: {
-        title: 'Erased drawings',
-        link: '/works/erased_drawings.html',
-        imageRootSrc: 'erased_drawings',
-        images: [
-            {src: 'erased_drawings_no4.jpg', title: 'Erased drawings (no. 4)', description: '2014. Graphite and walnut oil on paper, 18 x 24 inches.'},
-            {src: 'erased_drawings_no7.jpg', title: 'Erased drawings (no. 7)', description: '2014. Graphite on paper, 18 x 24 inches.'},
-            {src: 'erased_drawings_no6.jpg', title: 'Erased drawings (no. 6)', description: '2014. Graphite on paper, 18 x 24 inches.'},
-            {src: 'erased_drawings_no2.jpg', title: 'Erased drawings (no. 2)', description: '2014. Graphite and walnut oil on paper, 18 x 24 inches.'}
-        ]
-    },
+  other_works: {
+    title: 'Other works',
+    link: '/works/other_works.html',
+    imageRootSrc: 'other_works',
+    images: [
+      {src: 'other_works_floating_noah.jpg', title: 'Floating Noah', description: '2017. Oil and graphite on canvas, 20 x 16 inches.'},
+      {src: 'other_works_one_year.jpg', title: 'One year and four months', description: '2017. Graphite transfers and colored pencil on paper, 19 x 22 inches.'},
+      {src: 'other_works_moonrise.jpg', title: 'Moonrise', description: '2017. Graphite transfer on paper, 16 x 20 inches.'},
+      {src: 'other_works_noah.jpg', title: 'Noah', description: '2016. Watercolor and graphite on paper, 13 x 16 inches.'},
+      {src: 'other_works_everything_is_empty.jpg', title: 'Everything is empty', description: '2015. Screenprint, Loose graphite, 20 x 30 inches.'},
+      {src: 'other_works_monoprint.jpg', title: 'Monoprint', description: '2014. Screenprint, 15 x 14 inches.'}
+    ]
+  }
 
-    other_works: {
-        title: 'Other works',
-        link: '/works/other_works.html',
-        imageRootSrc: 'other_works',
-        images: [
-            {src: 'other_works_moonrise.jpg', title: 'Moonrise', description: '2017. Graphite transfer, 16 x 20 inches.'},
-            {src: 'other_works_noah.jpg', title: 'Noah', description: '2016. Watercolor and graphite on paper, 13 x 16 inches.'},
-            {src: 'other_works_everything_is_empty.jpg', title: 'Everything is empty', description: '2015. Screenprint, Loose graphite, 20 x 30 inches.'},
-            {src: 'other_works_monoprint.jpg', title: 'Monoprint', description: '2014. Screenprint, 15 x 14 inches.'}
-        ]
-    }
+}
 
-};
-
-
-module.exports = locals;
+module.exports = locals
